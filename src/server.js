5891425af6c693d1d3e4a1ef565a23b391990688
phaptran <phaptran112@gmail.com>
2021-09-13 03:48:19 +0700

    config mongodb

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 import express from 'express'
-import { mapOrder } from '*/utilities/sort.js'
+import { connectDB } from '*/config/mongodb'
+import { env } from '*/config/environment'
 
 const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+connectDB().catch(console.log)
 
 app.get('/', (req, res) => {
   res.end('<h1>Hello world uasascach</h1><hr/>')
 })
 
-app.listen(port, hostname, () => {
-  console.log(`Hello james, i am running at ${hostname}:${port}/`)
+app.listen(env.PORT, env.HOST, () => {
+  console.log(`Hello james, i am running at ${env.HOST}:${env.PORT}/`)
 })
