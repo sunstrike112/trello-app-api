6c2888f074e611bded12bc171bedb7eb88af9233
phaptran <phaptran112@gmail.com>
2021-09-13 00:40:10 +0700

    config babeljs

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { mapOrder } from '*/utilities/sort.js'
 
 const app = express()
 
@@ -6,7 +7,7 @@ const hostname = 'localhost'
 const port = 8017
 
 app.get('/', (req, res) => {
-  res.end('<h1>Hello world</h1><hr/>')
+  res.end('<h1>Hello world uasascach</h1><hr/>')
 })
 
 app.listen(port, hostname, () => {
